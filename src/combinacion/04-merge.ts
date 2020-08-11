c9a13858acbd98d4d3d5b7f8c67a52fca382e24d
José Carlos Palma <palmahn@gmail.com>
2020-08-11 00:52:11 -0600

    Ejemplo de la función merge para combinar observables.

---
+++ src/combinacion/04-merge.ts
@@ -0,0 +1,12 @@
+import { fromEvent, merge } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+import { } from 'rxjs/ajax';
+
+const keyup$ = fromEvent(document, 'keyup');
+const click$ = fromEvent(document, 'click');
+
+
+merge(
+    keyup$.pipe(pluck('type')),
+    click$.pipe(pluck('type'))
+).subscribe(console.log);
